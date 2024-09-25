29ba1d1363435c3ddbef26e70892ed428c642b77
sabfrezz <sabir2007spartak@mail.ru>
2024-09-25 09:25:16 +0300

    Create kodispravlen.cpp

---
+++ kodispravlen.cpp
@@ -0,0 +1,59 @@
+#include
+#include
+#include
+using namespace std;
+int main()
+{
+	int a1 = 60; int a2 = 60; int b1 = 25; int c1 = a1 & b1;
+	int c2 = a1 | b1; int c3 = a1 -- b1; int c4 = a1 ++ b1; int c5 = a1 != b1;
+	int i = 0, b = 5;
+	if (c1 c3)
+	{
+		cout << "true" << endl;;
+	}
+
+	if (a1 == 3);
+	{
+		cout << "" << endl;
+		cin >> b1;
+		cout << " " << endl;
+		cin >> c1;
+		cout << " " << s1 << endl;
+	}
+	if (q1 !5)
+	{
+		cout << "" << endl;
+		cin >> w1;
+		cout << "" << endl;
+		cin >> e1;
+		r1 = ; cout << "" << r1 << endl;
+	}
+	if (a1 > b1 c2 < c1)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+	if (c5 c3 || c5 c3)
+	{
+		cout << "true" << endl;
+	}
+	else
+	{
+		cout << "false" << endl;
+	}
+
+	while (i < 10 ++b < 10)
+	{
+		i++;
+		i1 = i1 + i1;
+		i2 = i2 + i2;
+		cout << i1 << endl;
+		cout << i2 << endl;
+		b++;
+	}
+}
+}
+}
